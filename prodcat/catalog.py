8d244694c915b30a3eca009f6c0024c2d82d376e
Saibaba Telukunta <saibaba.telukunta@gmail.com>
2012-07-23 08:39:42 -0500

    updates

---
+++ prodcat/catalog.py
@@ -0,0 +1,90 @@
+from api.graph import Db, Node
+import webapp2
+import json
+
+class VersionHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.out.write(dict(versions=[dict(id="1.0", status="BETA")]))
+
+def _catalog_root():
+    db = Db()
+    ref = db.reference_node
+    for r in ref.relationships.outgoing['CATALOGS_REF']:
+        return r.end()
+    else:
+        return ref.relationships.create("CATALOGS_REF", Node(name="CATALOGS_ROOT"))
+
+class Catalogs(webapp2.RequestHandler):
+
+    def post(self):
+
+        ref = _catalog_root()
+
+        data = self.request.body
+        req_json = json.loads(data)
+        catalog_name = req_json['catalog_name']
+
+        self.response.headers['Content-Type']  = "application/json"
+        self.response.headers['Location'] = "/catalog/" + str(catalog_name)
+
+        for r in ref.relationships.outgoing.CATALOG_REL:
+            if r.end().name == catalog_name:
+                self.response.status = "409 Conflict"
+                self.response.out.write("Catalog already created!")
+                return
+
+        ref.relationships.create("CATALOG_REL" , Node(name=catalog_name))
+        self.response.status = "201 Created"
+        self.response.out.write("Catalog created!")
+
+    def get(self):
+        #ref = _catalog_root()
+        self.response.out.write("Will list all")
+
+class Catalog(webapp2.RequestHandler):
+
+    def get(self, name=None):
+        #ref = _catalog_root()
+        if name is None:
+            self.response.out.write("Will list all")
+        else:
+            self.response.out.write("Will list " + name)
+
+application = webapp2.WSGIApplication(
+  [
+    ('/prodcat/catalogs', Catalogs),
+    ('/prodcat/catalogs/(.+)', Catalog), 
+  ] , debug=True)
+
+"""
+class Category(object):
+
+    def POST(self):
+        data = web.data()
+        print data
+        req_json = json.loads(data)
+                                        rv = PaymentMethod.create(req_json)
+                                                
+
+                                                        print "return value:", rv
+
+                                                                d = rv.value
+                                                                        content = _write(d)
+
+                                                                                if content is not None:
+                                                                                                return content
+
+                                                                                                def PUT(self):
+                                                                                                            data = web.data()
+                                                                                                                    req_json = json.loads(data)
+                                                                                                                            pm = PaymentMethod.find(req_json)
+                                                                                                                                    print "Find result:", pm
+                                                                                                                                            d = pm.value
+                                                                                                                                                    print "Find value:", d
+                                                                                                                                                            rv = d.update(req_json)
+                                                                                                                                                                    d = rv.value
+                                                                                                                                                                            print "update value:", d
+                                                                                                                                                                                    content = _write(d)
+                                                                                                                                                                                            if content is not None:
+                                                                                                                                                                                                            return content
+"""
