865f6907ccc11999ca6cf0a1eb5f54c0bbb2fe1d
Saibaba Telukunta <saibaba.telukunta@gmail.com>
2012-08-10 02:43:52 -0500

    fixed few things to run in appspot.com

--- samples/lib/blog.py
+++ samples/lib/blog.py
@@ -1,18 +1,24 @@
 from api.graph import Node
 
-jo4neo = Node.new('BLOG', name="jo4neo")
-welcome = Node.new('BLOG', name="welcome")
+tenant='myblog'
 
-summer = Node.new('BLOG', name="Summer")
-mark = Node.new('BLOG', name="Mark")
+jo4neo = Node.new(tenant, name="jo4neo")
+welcome = Node.new(tenant, name="welcome")
 
-post1 = Node.new('BLOG', content="jo4neo makes it easy to provision a graph with data")
+summer = Node.new(tenant, name="Summer")
+mark = Node.new(tenant, name="Mark")
+
+post1 = Node.new(tenant, content="jo4neo makes it easy to provision a graph with data")
 post1.relationships.outgoing.create("author", summer)
 post1.relationships.outgoing.create("hasTag", jo4neo)
 
-post2 = Node.new('BLOG', content="jo4neo provides simple yet useful features")
+post2 = Node.new(tenant, content="jo4neo provides simple yet useful features")
 post2.relationships.outgoing.create("author", mark)
 post2.relationships.outgoing.create("hasTag", welcome)
 post2.relationships.outgoing.create("hasTag", jo4neo)
 
+c = 0
+for r in post2.relationships.outgoing.hasTag:
+    c+=1
 
+assert c == 2
